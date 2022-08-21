e4486193b0f03aea87e78b78b3b3e78dfa8486cc
Gabriel Contarini <gcontarini@gmail.com>
2022-08-21 20:30:05 +0100

    Update deploy proxy contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('@openzeppelin/hardhat-upgrades');
 
 dotenv.config();
 
--- scripts/deployLiquidityStakingProtocol.ts
+++ scripts/deployLiquidityStakingProtocol.ts
@@ -1,22 +1,19 @@
-import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
+const { ethers, upgrades } = require("hardhat");
 
 require('dotenv').config()
 
 async function main() {
   const [deployer] = await ethers.getSigners()
   const owner = deployer.address
-  
-  // 
+
+  // const Box = await ethers.getContractFactory("Box");
+  // const box = await upgrades.deployProxy(Box, [42]);
+  // await box.deployed();
+  // console.log("Box deployed to:", box.address);
+
   const STAKEDAVAX = await ethers.getContractFactory("StakedAvax")
-  const sAvax = await STAKEDAVAX.deploy(owner)
+  const sAvax = await upgrades.deployProxy(STAKEDAVAX, [BigNumber.from("3000000000"), BigNumber.from("5000000000")])
   await sAvax.deployed()
   console.log("sAvax Token deployed at: ", sAvax.address)
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
