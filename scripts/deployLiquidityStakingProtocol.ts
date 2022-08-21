1fd6d6cf07da737f53ff1a4347673a1cc6a80ce6
Gabriel Contarini <gcontarini@gmail.com>
2022-08-21 20:34:38 +0100

    Remove example code

--- scripts/deployLiquidityStakingProtocol.ts
+++ scripts/deployLiquidityStakingProtocol.ts
@@ -7,11 +7,6 @@ async function main() {
   const [deployer] = await ethers.getSigners()
   const owner = deployer.address
 
-  // const Box = await ethers.getContractFactory("Box");
-  // const box = await upgrades.deployProxy(Box, [42]);
-  // await box.deployed();
-  // console.log("Box deployed to:", box.address);
-
   const STAKEDAVAX = await ethers.getContractFactory("StakedAvax")
   const sAvax = await upgrades.deployProxy(STAKEDAVAX, [BigNumber.from("3000000000"), BigNumber.from("5000000000")])
   await sAvax.deployed()
