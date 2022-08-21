093d3d30f7cb83db63eec41f14d2f6cc1eaecec1
Gabriel Contarini <gcontarini@gmail.com>
2022-08-21 19:20:03 +0100

    Add liquid staking protocol files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
    solidity: {
     compilers: [
       { version: "0.5.17" },
-      {version: "0.6.12" } 
+      { version: "0.6.12" } 
     ]
    },
    networks: {
